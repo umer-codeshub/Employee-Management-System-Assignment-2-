7c7aa5d996b65d557e451417574d78f4f92781b5
Umer Faryad <umar4444far@gmail.com>
2025-11-15 18:06:42 +0500

    Update Source.cpp
    Updated Employee class to use dynamic memory allocation for employeeId and employeeSalary. Adjusted constructors, display, and destructor methods accordingly.

--- Tech Solutions/Source.cpp
+++ Tech Solutions/Source.cpp
@@ -2,23 +2,26 @@
 using namespace std;
 class Employee
 {
-    string* employeeName;
-    int employeeId;
-    double employeeSalary;
+    string *employeeName;
+    int *employeeId;
+    double *employeeSalary;
     static int employeeCount;
     const static string companyName;
 
 public:
-    Employee(string name = "", int id = 0, double salary = 0) : employeeId(id), employeeSalary(salary) // Parameterized constructor & default constructor
+    Employee(string name = "", int id = 0, double salary = 0) // Parameterized constructor & default constructor
     {
+        this->employeeId = new int(id);
         this->employeeName = new string(name);
+        this->employeeSalary = new double(salary);
         employeeCount++;
     }
 
-    Employee(const Employee& old) // Copy constructor (deep copy)
+    Employee(const Employee &old) // Copy constructor (deep copy on name and id),  (shallow copy on salary)
+    // simply showing deep copy and shallow copy behavoiur in just 1 constructor
     {
         this->employeeName = new string(*old.employeeName);
-        this->employeeId = old.employeeId;
+        this->employeeId = new int(*old.employeeId);
         this->employeeSalary = old.employeeSalary;
         employeeCount++;
     }
@@ -34,40 +37,38 @@ public:
     }
     void setSalaray(double salary)
     {
-        this->employeeSalary = salary;
+        *employeeSalary = salary;
     }
     void setId(int id)
     {
-        this->employeeId = id;
+        *employeeId = id;
     }
 
     void display() const // Display function (const)
     {
-        cout << "\n-----------------------------" << endl;
+        cout << "-----------------------------" << endl;
         cout << "Company Name: " << companyName << endl;
         cout << "Employee Name: " << *employeeName << endl;
-        cout << "Employee ID: " << employeeId << endl;
-        cout << "Employee Salary: " << employeeSalary << endl;
-        cout << "-----------------------------" << endl;
+        cout << "Employee ID: " << *employeeId << endl;
+        cout << "Employee Salary: " << *employeeSalary << endl;
     }
 
     Employee returnCopy() const // Return copy of object
     {
         cout << "\nReturning object from a function!!!" << endl;
-        Employee temp(*this);
-        return temp;
+        return *this;
     }
 
-    void showData(const Employee& obj) // Show data by passing object
+    void showData(const Employee &obj) // Show data by passing object
     {
-        cout << "\nShowing Employee details using obj passed!!" << endl;
+        cout << "\nShowing Employee details by passing objects" << endl;
         obj.display();
     }
 
     ~Employee() // Destructor
     {
         delete employeeName;
-        cout << "\nEmployee deleted!!" << endl;
+        cout << "Employee deleted!!" << endl;
         employeeCount--;
         cout << "Remaining Employees: " << employeeCount << endl;
     }
@@ -79,11 +80,9 @@ int Employee::employeeCount = 0;
 int main()
 {
     cout << "Total employees at start: " << Employee::getEmployeeCount() << endl;
-
     Employee E1("Umer Faryad", 18, 50000);
     Employee E2(E1);
-
-    cout << "\nOriginal data!!" << endl;
+    cout << "Original data!!" << endl;
     E1.display();
     E2.display();
 
@@ -92,6 +91,7 @@ int main()
     E2.setId(2);
 
     cout << "\nModified data!!" << endl;
+    cout << "Deep copy on id and name, shallow copy on salary!!" << endl;
     E1.display();
     E2.display();
 
@@ -99,8 +99,11 @@ int main()
 
     Employee E3 = E1.returnCopy();
     E3.display();
+    cout << "\nDynamic allocated object!!!" << endl;
+    Employee *E = new Employee("Ali", 07, 55000);
+    E->display();
 
-    cout << "\nTotal employees at end: " << Employee::getEmployeeCount() << endl;
+    cout << "Total employees at end: " << Employee::getEmployeeCount() << endl;
 
     return 0;
 }
